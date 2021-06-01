fcb883f22e436f4ff97e42b65d5a78b6d19241ed
David Rodríguez <davidf.rodriguezw@konradlorenz.edu.co>
2021-06-01 16:29:42 -0500

    Se agregan los archivos fuente del proyecto

---
+++ JSpags/jsProyecto.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    //Ejecutamos método que oculta las cajas
+    OcultarContenedor1();
+  
+    //Cada 6 segundos ejecutamos la función que reinicializa los contenedorees
+    setInterval(function() {
+      OcultarContenedor2();
+    }, 3500);
+  
+  });
+  
+  //Método que oculta el primer contenedor para mostrar el otro
+  function OcultarContenedor1() {
+    setTimeout(function() {
+      $("#pantallaHome").hide();
+      $("#pantallaCantante").hide();
+      $("#pantallaConciertos").hide();
+      $("#pantallaBebidas").hide();
+      $("#pantallaFinal").hide();
+    });
+  
+    setTimeout(function() {
+      $("#pantallaDeCarga").show();
+    }, 3500);
+  }
+  //Método que reinicializa los contenedores
+  function OcultarContenedor2() {
+    $('#pantallaHome').show(1000);
+    $("#pantallaCantante").show(1000);
+    $("#pantallaConciertos").show(1000);
+    $("#pantallaBebidas").show(1000);
+    $("#pantallaFinal").show(1000);
+    $('#pantallaDeCarga').hide();
+  }
