7e44d0a283e8be7ac215b5ce4cdca0490eac8a6f
David Rodríguez <davidf.rodriguezw@konradlorenz.edu.co>
2021-06-03 15:28:45 -0500

    Se agregan modificaciones por Michael

---
+++ JSpags/jsContenido1.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    //Ejecutamos método que oculta las cajas
+    OcultarContenedor1();
+  
+    //Cada 6 segundos ejecutamos la función que reinicializa los contenedorees
+    setInterval(function() {
+      OcultarContenedor2();
+    }, 0);
+  
+  });
+  
+  //Método que oculta el primer contenedor para mostrar el otro
+  function OcultarContenedor1() {
+    setTimeout(function() {
+      $("#contenido").hide();
+      
+      
+    });
+  
+    setTimeout(function() {
+      $("#pantallaDeCarga").show();
+    }, 3500);
+  }
+  //Método que reinicializa los contenedores
+  function OcultarContenedor2() {
+    $('#contenido').show(1000);
+    $('#pantallaDeCarga').hide();
+  }
