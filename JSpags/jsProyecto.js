62b0255459d37a14a9b0db36b2e1e89bf1fa9240
DanielaSG <daniela.saganomeg@konradlorenz.edu.co>
2021-06-03 09:42:57 -0500

    Terminacion total de funciones y estilos.

--- JSpags/jsProyecto.js
+++ JSpags/jsProyecto.js
@@ -1,7 +1,7 @@
   $(document).ready(function() {
     //Ejecutamos método que oculta las cajas
     OcultarContenedor1();
-  
+    
     //Cada 6 segundos ejecutamos la función que reinicializa los contenedorees
     setInterval(function() {
       OcultarContenedor2();
@@ -32,10 +32,11 @@
     $("#pantallaFinal").show(1000);
     $('#pantallaDeCarga').hide();
   }
+  
   function caracteres(){
     var input = document.getElementById('nombre');
     if(input.value.length < 5) {
-    alert('Escribe más de 15 carácteres.');
+    alert('Escribe más de 5 carácteres.');
     }
     }
   function colorTitulos(){
@@ -56,3 +57,31 @@
       document.querySelector(".g").style.display = 'none';
     }
   }
+  $(function(){
+    $("#telefono").keydown(function(event){
+        let can =  document.getElementById('telefono').value;
+        //alert(event.keyCode);
+        if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105) && event.keyCode !==190  && event.keyCode !==110 && event.keyCode !==8 && event.keyCode !==9  ){
+            return false;
+        }
+    });
+  });
+  function numeros(){
+    var input = document.getElementById('telefono');
+    if(input.value.length < 7) {
+    alert('Escribe más de 7 numeros.');
+    }
+    }
+   
+    function validarMensaje() {
+      var mensaje = document.getElementById('mensaje');
+      var value = mensaje.value;
+      
+      if(!value){
+        alert('Este campo tiene que llenarlo');
+      } else if(value.length < 50){
+        alert('Este campo debe tener minimo 50 carácteres.')
+      } else if (value.length > 200){
+        alert('Este campo solo permite 200 carácteres.')
+      }
+    }
